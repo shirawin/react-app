153d280bedccafa927b0e5bb744753c4ff3880b6
Shira Vinograd <106039892+shirawin@users.noreply.github.com>
2023-02-16 21:27:22 +0200

    onloud page

--- my-app/src/Login/logIn.js
+++ my-app/src/Login/logIn.js
@@ -6,6 +6,9 @@ import { Grid } from '@mui/material';
 import { useNavigate } from 'react-router-dom'
 import { style } from '@mui/system';
 import {CheckingUser} from '../Api/Users_Api'
+import Alert from '@mui/material/Alert';
+import Swal from 'sweetalert2'
+
 
 const SignIn =()=>{
   
@@ -33,44 +36,52 @@ const handleSubmit = async (e) => {
     *\ */
   
   e.preventDefault();
-   // const resHelpeds = await CheckingUser(logIn.userName,logIn.password);
-// if(resHelpeds==-1){
-//   alert("שם משתמש וסיסמה נכונים+משתמש לא פעיל");
-// }
-//   else{
-//     if(resHelpeds==1){
-//       alert("סיסמה שגויה ")
-//     }
-//     else{
-//       if(resHelpeds==2)
-//       alert("משתמש לא קיים")
-//       else{
-//          navigate("/ManagePage")
-//       }
+  debugger
+  const resHelpeds = await CheckingUser(logIn.userName,logIn.password);
+   if(resHelpeds==-1){
+  alert("שם משתמש וסיסמה נכונים+משתמש לא פעיל");
+   }
+  else{
+    if(resHelpeds==1){
+      alert("סיסמה שגויה ")
+    }
+    else{
+      if(resHelpeds==2){
+        alert("משתמש לא קיים")
+        navigate("/SignUp")
 
-//     }
-//   }
+      }
+      // else{
+      //    navigate("/ManagePage")
+      // }
+
+    }
+  }
         navigate("/ManagePage")
 
 
       localStorage.setItem("uuid", 9);
-   
-   
+  }
+  const navigateToSignup=()=>{
+    navigate("/SignUp")
   }
 
-return (
-   <div style={{backgroundColor:"pink"}}>
-    <Box component="form" onSubmit={handleSubmit} dir='rtl' >
-        <h1>כניסה למערכת</h1>
-        <div className='div-personal-detailes'>
-        <TextField id="outlined-basic" label="שם" variant="outlined"   onChange={(e) => setLogIn({ ...logIn, userName: e.target.value})}/>
-        <TextField id="outlined-basic" label="סיסמה" variant="outlined"   type='password' onChange={(e) => setLogIn({ ...logIn, password: e.target.value})}/>
-        </div>
 
+return (
+   <div className='login'>
+    <Box component="form" onSubmit={handleSubmit} dir='rtl' >
+        <h1 className='title'>התחברות</h1>
+        <h2 className="signup">פעם ראשונה באתר? <span className='link' onClick={navigateToSignup}>הרשמה</span></h2>
+          <div className='name'>
+          <TextField id="outlined-basic" label="שם" variant="outlined"   onChange={(e) => setLogIn({ ...logIn, userName: e.target.value})}/>
+          </div>
+          <div className='pass'>
+          <TextField id="outlined-basic" label="סיסמה" variant="outlined"   type='password' onChange={(e) => setLogIn({ ...logIn, password: e.target.value})}/>
+          </div>
         <div className='ok-div-Wrap'>
-                    <span></span>
-                    <Button style={{backgroundColor: '#ff9100'}} className='ok' variant="contained" type='submit'>אישור</Button>
-    </div>
+             <span></span>
+             <Button style={{backgroundColor: '#ff9100'}} className='ok' variant="contained" type='submit'>אישור</Button>
+        </div>
     </Box>
    </div>
     
