7f226596df291a4b98fcd735675557951e830f99
avitalush <106091147+avitalush@users.noreply.github.com>
2023-05-23 00:58:04 +0300

    23/05/23

--- my-app/src/PersonalArea/PersonalArea.js
+++ my-app/src/PersonalArea/PersonalArea.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import AppBar from '@mui/material/AppBar';
+import React, { useEffect } from 'react'
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
+
 import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import Details from './Details'
 import Request from '../ManagePage/Helped/request'
-import Adv from '../ManagePage/Helped/adv'
 import  './PersonalArea.css'
 import { useSelector } from 'react-redux';
 import ManagePage from '../ManagePage/ManagePage';
@@ -18,23 +16,25 @@ const PersonalArea =()=>{
     const handleChange = (event, newValue) => {
         setValue(newValue);
       };
- 
       const [value, setValue] = React.useState('1');
       const user = useSelector((state) => state.user);
       return (
    
     <Box sx={{ width: '100%', typography: 'body1' }}   >
-    {user.userType&&
+    
+    {user.usertype&&
+  <>
     <TabContext value={value} >
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}id="heade">
         <TabList onChange={handleChange} aria-label="lab API tabs example" id="bra">
           <Tab label="עדכון פרטים" value="1" />
         </TabList>
       </Box>
-       <TabPanel value="1" ><Details/></TabPanel>
+       <TabPanel value="1" > <Details screen={true} header={'עדכון פרטים'} type={user.usertype}/> </TabPanel>
     </TabContext>
+    </>
      }
-     {!user.Usertype&&
+     {!(user.usertype)&&
     <TabContext value={value} >
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}id="heade">
         <TabList onChange={handleChange} aria-label="lab API tabs example" id="bra">
@@ -46,8 +46,7 @@ const PersonalArea =()=>{
       <TabPanel value="1"><Request/></TabPanel> 
       <TabPanel value="2">
         <div className='tabDetailes'>
-          <h1>עדכון פרטים אישיים</h1>
-          <Details userType={1}/>
+          <Details screen={true} header={'עדכון פרטים'} type={user.usertype}/>
         </div>
         </TabPanel>
      
